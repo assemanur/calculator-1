50fe188f46e8b0745fe90edef717644fe5b34fb8
assemanur <asemyara.n@gmail.com>
2022-11-09 12:18:45 -0800

    Added adding 3 numbers and adding cubes of two numbers functions

--- arithmetic.py
+++ arithmetic.py
@@ -37,7 +37,14 @@ def power(num1, num2):
     """Raise num1 to the power of num2 and return the value."""
     return num1 ** num2
 
-
 def mod(num1, num2):
     """Return the remainder of num1 / num2."""
     return num1 % num2
+
+def add_mult(num1, num2, num3):
+    """Get the sum of num1 and num2, then multiply sum with num3."""
+    return (add(num1,num2) * num3)
+
+def add_cubes(num1, num2):
+    """Add the cubes of num1 and num2"""
+    return add(cube(num1), cube(num2))
